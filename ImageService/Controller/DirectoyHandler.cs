9aa5142f169562ef97c436d576b6b69dc6e89129
Jed-El <jed.el.me@gmail.com>
2018-04-10 00:29:09 +0300

    I start working on the handler

--- ImageService/Controller/DirectoyHandler.cs
+++ ImageService/Controller/DirectoyHandler.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using ImageService.Logging;
 using System.Text.RegularExpressions;
 using ImageModel;
+using ImageService.Infrastructure.Enums;
 
 namespace ImageService.Controller.Handlers
 {
@@ -36,17 +37,54 @@ namespace ImageService.Controller.Handlers
         public void StartHandleDirectory(string dirPath)
         {
             m_dirWatcher.Path = dirPath;
-            //we will be filtering nothing because we need to watch multiple types, filtering will be done on event.
-            //this is supposed to be more efficient than having 4 watchers to each folder.
-            m_dirWatcher.Filter = "*.*";
-        }
+			m_dirWatcher.Created += new FileSystemEventHandler(OnChanged);
 
-        public void OnCommandRecieved(object sender, CommandRecievedEventArgs e)
-        {
-            Task t = new Task (() =>
-            {
-                m_controller.ExecuteCommand()
-            })
-        }
+			//we will be filtering nothing because we need to watch multiple types, filtering will be done on event.
+			//this is supposed to be more efficient than having 4 watchers to each folder.
+			m_dirWatcher.Filter = "*.*";
+
+			// Start monitoring
+			m_dirWatcher.EnableRaisingEvents = true;
+
+		}
+
+		// Define the event handlers.
+		private static void OnChanged(object source, FileSystemEventArgs e)
+		{
+			// Specify what is done when a file is changed, created, or deleted.
+			switch (e.ChangeType)
+			{
+				case WatcherChangeTypes.Created:
+					OnCommandRecieved(source,
+						new CommandRecievedEventArgs((int)CommandEnum.NewFileCommand, new string[] {e.FullPath}, e.FullPath));
+					break;
+				default:
+					return;
+			}	
+		}
+
+
+		public void OnCommandRecieved(object sender, CommandRecievedEventArgs e)
+		{
+			// check if command is meant for its directory
+			if (!e.RequestDirPath.Equals(m_path))
+				return;
+			// handle command
+			Task t = new Task(() =>
+		   {
+			   // update logger of new command
+			   m_logging.Log("DirectoyHandler received a " + Enum.GetName(typeof(CommandEnum), e.CommandID),
+					MessageTypeEnum.INFO);
+			   bool check;
+			   // execute the command 
+			   string message = m_controller.ExecuteCommand(e.CommandID, e.Args, out check);
+			   // update logger with the result (success or failure)
+			   if (check)
+				   m_logging.Log("DirectoyHandler done with " + Enum.GetName(typeof(CommandEnum), e.CommandID),
+					   MessageTypeEnum.INFO);
+			   else
+				   m_logging.Log(message, MessageTypeEnum.FAIL);
+		   });
+		}
     }
 }
