c35f21489039a604e21989ea4c43d7d9c42bd367
Jed-El <jed.el.me@gmail.com>
2018-04-09 18:10:52 +0300

    little fix

--- ImageService/Commands/NewFileCommand.cs
+++ ImageService/Commands/NewFileCommand.cs
@@ -10,18 +10,18 @@ namespace ImageService.Commands
 {
     public class NewFileCommand : ICommand
     {
-        private IImageModel m_modal;
+        private IImageModel m_model;
 
-        public NewFileCommand(IImageModel modal)
+        public NewFileCommand(IImageModel model)
         {
-            m_modal = modal;            // Storing the Modal
+            m_model = model;            // Storing the Model
         }
 
         public string Execute(string[] args, out bool result)
         {
             // The String Will Return the New Path if result = true, and will return the error message
             // assuming the first argument is the path we pass on args[0]
-            return m_modal.AddFile(args[0], out result);
+            return m_model.AddFile(args[0], out result);
         }
     }
 }
