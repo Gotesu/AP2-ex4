694e2903753af8a37182220ab180310f293bae22
Gotesu <gotesuriel@gmail.com>
2018-06-21 09:00:12 +0300

    service into application

--- ImageService/Program.cs
+++ ImageService/Program.cs
@@ -12,15 +12,17 @@ namespace ImageService
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        
+
+        /*
         static void Main(string[] args)
         {
+            
              ServiceBase[] ServicesToRun = new ServiceBase[] { new ImageService(args) };
             ServiceBase.Run(ServicesToRun);
          }
+         */
 
-
-        /* the Method which was turned into a comment is an alternatice main for testing
+        // the Method which was turned into a comment is an alternatice main for testing
         static void Main(string[] args)
         
         {
@@ -35,7 +37,6 @@ namespace ImageService
                 ServiceBase.Run(ServicesToRun);
             }
         }
-        */
         
         
     }
